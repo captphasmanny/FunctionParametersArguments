166d1ececc2a82df947acb06db69aadd9d7bc89e
Manny <captphasmanny@att.net>
2023-11-07 15:04:15 -0500

    More practice.

--- FunctionParametersArguments/main.cpp
+++ FunctionParametersArguments/main.cpp
@@ -7,8 +7,82 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+/*void doPrint()
+{
+    std::cout << "In doPrint()\n";
+}
+
+void printValue(int x)
+{
+    std::cout << x <<'\n';
+}
+
+int add(int x, int y)
+{
+    return x + y;
+}
+
+int main()
+{
+    int num{std::cout << printValue(7)};
+    std::cout <<  << '\n';
+}
+
+int getValueFromUser()
+{
+    std::cout << "Enter an integer: ";
+    int input{};
+    std::cin >> input;
+    
+    return input;
+}
+void printDouble(int value)
+{
+    std::cout << value << " doubled is: " << value*2 << '\n';
+}
+
+int main ()
+{
+    printDouble(getValueFromUser());
+    return 0;
+}
+
+int getNum()
+{
+    std::cout << "Please provide a number: ";
+    int a{};
+    std::cin >> a;
+    return a;
+}
+
+void doubleNum(int x)
+{
+    std::cout << x << " doubled is: " << x*2 << '\n';
+    
+}
+
+int main()
+{
+    doubleNum(getNum());
+    return 0;
+}*/
+
+int getInteger()
+{
+    std::cout << "Please Enter a Number: ";
+    int a{};
+    std::cin >> a;
+    
+    return a;
+}
+
+void printInteger(int b)
+{
+    std::cout << b << " doubled is: " << b*2 << '\n';
+}
+
+int main()
+{
+    printInteger(getInteger());
     return 0;
 }
